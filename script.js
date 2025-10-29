46841c2435334aa763d066096b439bc8bd6493d2
handesaurabh <handesaurabh2004@gmail.com>
2025-10-29 11:09:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
 //your JS code here. If required.
-document.getElementById("myForm").addEventListener(function (e))
-e.preventDefault();
-const name=document.getElementById("name");
-const age=document.getElementById("age");
-if (name==="" || age==="") {
-	alert("Please enter valid details.");
-	return;
-}
-
-const age=()=>{
-	return new Promise(resolve,reject)=>{
-		setTimeout(()=>{
-			if (age>=18) {
-				resolve("Welcome, . You can vote.")
-			} else {
-				reject("Oh sorry . You aren't old enough.")
-			}
-		},4000)
-	}
-	age
-	.then((message)=>alert(message))
-	.catch((error)=>alert(error))
-}
+document.getElementById("myForm").addEventListener("submit", function (e) {
+  e.preventDefault(); 
+  const name = document.getElementById("name").value.trim();
+  const age = document.getElementById("age").value.trim();
+  if (name === "" || age === "") {
+    alert("Please enter valid details.");
+    return;
+  }
+ const checkAge = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (age >= 18) {
+        resolve(`Welcome, ${name}. You can vote.`);
+      } else {
+        reject(`Oh sorry ${name}. You aren't old enough.`);
+      }
+    }, 4000);
+  });
+  checkAge
+    .then((message) => alert(message))
+    .catch((error) => alert(error));
+});
 	
