d220c2b59dff1a30a292712942ea082931f438b2
handesaurabh <handesaurabh2004@gmail.com>
2025-10-29 11:07:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+document.getElementById("myForm").addEventListener(function (e))
+e.preventDefault();
+const name=document.getElementById("name");
+const age=document.getElementById("age");
+if (name==="" || age==="") {
+	alert("Please enter valid details.");
+	return;
+}
+
+const age=()=>{
+	return new Promise(resolve,reject)=>{
+		setTimeout(()=>{
+			if (age>=18) {
+				resolve("Welcome, . You can vote.")
+			} else {
+				reject("Oh sorry . You aren't old enough.")
+			}
+		},4000)
+	}
+	age
+	.then((message)=>alert(message))
+	.catch((error)=>alert(error))
+}
+	
